8ef92b5fc8ca5c0679d48b1e8912e1ad25a41fdc
Sangjae Bae <sbae@lbl.gov>
2019-05-02 11:44:35 -0700

    updating

--- init_params.m
+++ init_params.m
@@ -1,9 +1,10 @@
 function par = init_params()
 % initialize parameters
-par.operating_hours = 10;                   % operating hours
-par.station.num_poles = 10;                 % number of charging poles
+par.operating_hours = [];                   % operating hours
+par.station.num_poles = [];                 % number of charging poles
 par.TOU = [];                               % TOU price
-par.dcm.charging_flex.params = [-0.1 0.2];  % DCM parameters for choice 1 -- charging with flexibility 
-par.dcm.charging_asap.params = [-0.1 0.2];  % DCM parameters for choice 2 -- charging as soon as possible
-par.dcm.leaving.params = [0 0];             % DCM parameters for choice 3 -- leaving without charging
+par.dcm.charging_flex.params = [];  % DCM parameters for choice 1 -- charging with flexibility 
+par.dcm.charging_asap.params = [];  % DCM parameters for choice 2 -- charging as soon as possible
+par.dcm.leaving.params = [];             % DCM parameters for choice 3 -- leaving without charging
+par.pdf.visiting_rate = [];
 end
